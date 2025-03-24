d7f402a697911e2f8c256f909ad8f5477f117bad
Jeffrey <jeffreygaineswrk@gmail.com>
2025-03-23 21:00:24 -0400

    Added Home page skeleton

--- src/main/java/src/LunariApplication.java
+++ src/main/java/src/LunariApplication.java
@@ -2,10 +2,8 @@ package src;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class LunariApplication {
 
     public static void main(String[] args) {
